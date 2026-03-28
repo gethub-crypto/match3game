862cdf83326029244fc0918a7aafa20e09d3d56a
gethub-crypto <fernadester761@gmail.com>
2026-03-28 20:15:29 +0800

    Add game initialization and board creation functions

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function init() { console.log('Game started'); updateScreens(); createBoard(); }
+
+function createBoard() { const board = document.getElementById('board'); board.innerHTML = '';
+
+for (let i = 0; i < 64; i++) { const cell = document.createElement('div'); cell.className = 'cell'; 
+                              board.appendChild(cell); } }
+
+window.onload = init;
