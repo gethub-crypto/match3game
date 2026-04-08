dba214cafdc51285be21c16e034c62d26602511a
gethub-crypto <fernadester761@gmail.com>
2026-04-08 04:16:19 +0300

    Update matchDetection.js

--- js/matchDetection.js
+++ js/matchDetection.js
@@ -2,85 +2,140 @@ const MatchDetection = {
 
 getMatches(board){
 
-const matches=[]
-const size=board.length
+const size = board.length
+const matches = []
+const mark = new Set()
 
-const used=new Set()
-
-function key(x,y){
-return x+"_"+y
-}
+function key(x,y){ return x+"_"+y }
 
 function getColor(cell){
 if(!cell) return null
-if(typeof cell==="object") return cell.color || null
+if(typeof cell==="object") return cell.color
 return cell
 }
 
+// ===== горизонтальные линии =====
 for(let y=0;y<size;y++){
-for(let x=0;x<size;x++){
+
+let x=0
+
+while(x<size){
 
 const color=getColor(board[y][x])
-if(!color) continue
+if(!color){ x++; continue }
 
-let horiz=[{x,y}]
+let start=x
 
-for(let i=x+1;i<size;i++){
-
-if(getColor(board[y][i])===color){
-horiz.push({x:i,y})
-}else break
-
+while(x<size && getColor(board[y][x])===color){
+x++
 }
 
-if(horiz.length>=3){
+let len=x-start
 
+if(len>=3){
+
+let cells=[]
+
+for(let i=start;i<x;i++){
+cells.push({x:i,y})
+}
+
+// определяем тип
 let type=null
 
-if(horiz.length===4) type="rocket"
-if(horiz.length>=5) type="color"
+if(len===4) type="rocket"
+if(len>=5) type="color"
 
-let cells=[...horiz]
+// проверка T / L
+cells.forEach(c=>{
 
-horiz.forEach(c=>{
+let up=0
+let down=0
 
-let vert=[c]
-
-for(let j=c.y+1;j<size;j++){
-
-if(getColor(board[j][c.x])===color){
-vert.push({x:c.x,y:j})
-}else break
-
+for(let yy=c.y-1;yy>=0;yy--){
+if(getColor(board[yy][c.x])===color) up++
+else break
 }
 
-for(let j=c.y-1;j>=0;j--){
-
-if(getColor(board[j][c.x])===color){
-vert.unshift({x:c.x,y:j})
-}else break
-
+for(let yy=c.y+1;yy<size;yy++){
+if(getColor(board[yy][c.x])===color) down++
+else break
 }
 
-if(vert.length>=3){
-
+if(up+down>=2){
 type="bomb"
 
-vert.forEach(v=>cells.push(v))
-
+for(let i=1;i<=up;i++) cells.push({x:c.x,y:c.y-i})
+for(let i=1;i<=down;i++) cells.push({x:c.x,y:c.y+i})
 }
 
 })
 
-cells=cells.filter((c,i,a)=>
+// удаляем дубликаты
+cells = cells.filter((c,i,a)=>
+a.findIndex(o=>o.x===c.x && o.y===c.y)===i
+)
+
+// ключ группы
+const id=cells.map(c=>key(c.x,c.y)).join("|")
+
+if(!mark.has(id)){
+
+mark.add(id)
+
+matches.push({
+type:type,
+cells:cells
+})
+
+}
+
+}
+
+}
+
+}
+
+// ===== вертикальные линии =====
+for(let x=0;x<size;x++){
+
+let y=0
+
+while(y<size){
+
+const color=getColor(board[y][x])
+if(!color){ y++; continue }
+
+let start=y
+
+while(y<size && getColor(board[y][x])===color){
+y++
+}
+
+let len=y-start
+
+if(len>=3){
+
+let cells=[]
+
+for(let i=start;i<y;i++){
+cells.push({x,y:i})
+}
+
+let type=null
+
+if(len===4) type="rocket"
+if(len>=5) type="color"
+
+cells = cells.filter((c,i,a)=>
 a.findIndex(o=>o.x===c.x && o.y===c.y)===i
 )
 
 const id=cells.map(c=>key(c.x,c.y)).join("|")
 
-if(!used.has(id)){
+if(!mark.has(id)){
 
-used.add(id)
+mark.add(id)
 
 matches.push({
 type:type,
@@ -99,4 +154,4 @@ return matches
 
 }
 
-    }
+      }
